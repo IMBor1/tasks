cf61bc303173a7642b803f1d7d02c1c1eb92765f
IMBor1 <ilacerber1@gmail.com>
2024-10-20 20:48:27 +0300

    ComplexTaskExecutor

--- src/main/java/com/test/tasks/cuncurrency/ComplexTask.java
+++ src/main/java/com/test/tasks/cuncurrency/ComplexTask.java
@@ -1,21 +1,18 @@
 package com.test.tasks.cuncurrency;
 
-public class ComplexTask {
-    private Long num1 = 100L;
-    private Long num2 = 150L;
 
-    private Long count1 = 0L;
-    public Long count2 = 0L;
+import java.util.concurrent.Callable;
 
-    public Long countNum1(Long num1) {
-        new Thread()
-        for (int i = 0; i < num1; i++) {
-            count1 += i;
-        } return count1;
+public class ComplexTask implements Callable<String> {
+    private final int taskId;
+
+    public ComplexTask(int taskId) {
+        this.taskId = taskId;
     }
-    public Long countNum2(Long num2) {
-        for (int i = 0; i < num2; i++) {
-            count2 += i;
-        } return count2;
+
+    @Override
+    public String call() throws Exception {
+        Thread.sleep((long) (Math.random() * 1000));
+         return "Результат задачи " + taskId;
     }
 }
